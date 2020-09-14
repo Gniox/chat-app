6e1d7a58d33c8167f4166b5faaa41c90e2ed0479
Chaly Xiong <gniox15@gmail.com>
2020-09-14 00:13:32 -0500

    sendfile and the html structure

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var app = require('express')();                //express initializes app to be given to http server
 var http = require('http').createServer(app);  //http server
 
-app.get('/', (req, res) => {                   //define route handler '/' that gets called
-    res.send('<h1>Hello world</h1>');          //when we hit website home
+// app.get('/', (req, res) => {                   //define route handler '/' that gets called
+//     res.send('<h1>Hello world</h1>');          //when we hit website home
+// })
+
+app.get('/', (req, res) => {                   //passover an html file 
+    res.sendFile(__dirname + '/index.html');
 })
 
 http.listen(3000, () => {                      //make http server listen on port 3000

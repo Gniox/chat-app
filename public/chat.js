9507d5d5e78d452c0dd60cdce52368e6478faffa
Chaly Xiong <gniox15@gmail.com>
2020-09-15 19:07:16 -0500

    event upon entering, messages working

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,7 @@
 const socket = io();
+let prevName = '';
+let curName = '';
+let hasName = false;
 
 document.getElementById("m").addEventListener("keydown", function (event) {
   if (event.code === "Enter") {
@@ -6,15 +9,45 @@ document.getElementById("m").addEventListener("keydown", function (event) {
   }
 });
 
-socket.on('chat message', function(msg) {
-  let message = document.createElement('li');
-  message.appendChild(document.createTextNode(msg));
-  document.getElementById('messages').append(message);
-  document
-})
+document.getElementById("name").addEventListener("keydown", function (event) {
+  if (event.code === "Enter") {
+    if (document.getElementById("name").value != '') submitName();
+  }
+});
+
+// document.getElementById("name").addEventListener("mouseout", function () {
+//   if (document.getElementById("name").value != '') {
+//     curName = document.getElementById("name").value;
+//     submitName();
+//   }
+    
+// });
+
+socket.on("entered", (name) => {
+  let message = document.createElement("li");
+  message.appendChild(
+    document.createTextNode(name + " has entered the chat UwU")
+  );
+  document.getElementById("messages").append(message);
+});
+
+socket.on("chat message", (msg) => {
+  let message = document.createElement("li");
+  let name = document.getElementById("name").value + ": ";
+  message.appendChild(document.createTextNode(name + msg));
+  document.getElementById("messages").append(message);
+});
 
 function submitMessage() {
-  event.preventDefault();
-  socket.emit("chat message", document.getElementById("m").value);
-  document.getElementById("m").value = "";
+  if (document.getElementById("name").value == '') {
+    alert("Name yourself first.");
+  } else {
+    event.preventDefault();
+    socket.emit("chat message", document.getElementById("m").value);
+    document.getElementById("m").value = "";
+  }
+}
+
+function submitName() {
+  socket.emit("entered", document.getElementById("name").value);
 }
